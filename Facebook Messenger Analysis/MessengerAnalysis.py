0ecc107779c30b9d9b74a060c50baf03d8ce9712
ben-dow <marsmandow@gmail.com>
2018-06-02 23:36:54 -0400

    changed MessengerAnalysis to have a console prompt

--- Facebook Messenger Analysis/MessengerAnalysis.py
+++ Facebook Messenger Analysis/MessengerAnalysis.py
@@ -3,15 +3,13 @@ import time
 
 from DataBuilder.ChatParsing import ChatHistory
 
-'''
+
 print("What is the Location of the File?")
 file_location = input()
 
 print("What is the full facebook name of the user that downloaded this data?")
 data_owner = input()
-'''
-file_location = "C:\\Users\Benjamin Dow\Desktop\BAEtchyasevah_732c7ad1d8\message.json"
-data_owner = "Benjamin Dow"
+
 
 with open(file_location) as file:
     data = json.load(file)
